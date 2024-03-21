64e926e477e67777ef79cb43825b133c8c662470
Jardon <javier.jar23rosas@gmail.com>
2024-03-20 23:23:09 -0600

    Home responsive, PP RESPONSIVE, PAGIAN DE CONTACTO

---
+++ assets/js/index.js
@@ -0,0 +1,16 @@
+
+
+
+$('.IconMovil').click(function () {
+    $(".menumovil").css({
+      "right": "0%"
+    });
+  })  
+
+
+
+  $('.CerrarCosas').click(function () {
+    $(".menumovil").css({
+      "right": "-80%"
+    });
+  })  
